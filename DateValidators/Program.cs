b5da2eb7aa5339c7adc4888ad1582f557a2632f0
vicktor123 <peshkovct@gmail.com>
2023-12-17 22:47:06 +0300

    commit FunctionDate

---
+++ DateValidators/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DateValidators
+{
+
+    public static class DateValidators
+    {
+        public static bool IsWeekend(DateTime dateTime)
+            => dateTime.DayOfWeek == DayOfWeek.Saturday ||
+               dateTime.DayOfWeek == DayOfWeek.Sunday;
+
+        public static bool IsFuture(DateTime dateTime)
+            => dateTime.Date > DateTime.Today;
+    }
+
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            
+            
+
+        }
+    }
+}
