090707ab92479c8905f7f927f6d4b76f651d214f
vicktor123 <peshkovct@gmail.com>
2023-12-17 23:41:37 +0300

    all today

---
+++ EmailFormatte/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EmailFormatte
+{
+    
+
+    internal class Program
+    {
+        private delegate string EmailFormatter(string email);
+        private static void testing1()
+        {
+            EmailFormatter emailFormatter = new EmailFormatter(RemoveDots);
+
+            const string Address = "admin@google.com";
+            var first = emailFormatter(Address);
+            Console.WriteLine($"First={first}");
+            emailFormatter += RemoveAtSign;
+
+            var second = emailFormatter(Address);
+            Console.WriteLine($"Second={second}");
+            
+        }
+        static void Main(string[] args)
+        {
+            testing1();
+        }
+    }
+}
