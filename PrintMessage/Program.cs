071320aadbefaf9f70945edae9238003a44e4a22
vicktor123 <peshkovct@gmail.com>
2023-12-26 22:38:34 +0300

    26,12,23

---
+++ PrintMessage/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PrintMessage
+{
+    public delegate void Del(string msg);
+    class Class6
+    {
+        public void Method(int param1,Del callback)
+        {
+            callback("The number is : " + param1.ToString());
+        }
+    }
+    internal class Program
+    {
+        static void PrintMes(string mes)
+        {
+            Console.WriteLine(mes);
+        }
+        static void Main(string[] args)
+        {
+            Class6 cl = new Class6();
+            Del deleg = new Del(PrintMes);
+
+            cl.Method(3, deleg);
+            Console.ReadKey();
+        }
+    }
+}
